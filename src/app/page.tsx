5792334185f345adcbf9d57e5040be4b23eb22a0
zinx | Tasdid <tasdid110@gmail.com>
2023-03-18 19:05:15 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+'use client'
 import Image from "next/image";
+import { useEffect } from "react"
+import { useRouter } from 'next/navigation';
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(()=>{
+  router.push('/store')
+  },[])
   return <>main</>;
 }
